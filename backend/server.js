fa9ec28ce12cb8115899b0d92fc84c546adb354b
Isabela Schmidt <belacayres.schmidt@gmail.com>
2020-11-18 08:49:44 -0300

    aula 6

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,11 @@
-const express = require('express');
-const products = require('./data/products');
+import express from 'express';
+import connectDB from './config/db.js';
+import colors from 'colors';
+import dotenv from 'dotenv';
+import Product from './models/productModel.js';
+
+dotenv.config();
+connectDB();
 
 const app = express();
 
@@ -7,13 +13,24 @@ app.get('/', (req, res) => {
   res.send({ message: 'API está funcionando!', data: 'oi' });
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
+app.get('/api/products', async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.json(products);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
+app.get('/api/products/:id', async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ message: 'Produto não encontrado!' });
+  }
 });
 
-app.listen(5000, console.log('Servidor iniciado na porta 5000'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Servidor iniciado na porta ${PORT}`.green.bold));
